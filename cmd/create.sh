4856f3454522bb96a03b145af237855c45ef7d59
Dashamir Hoxha <dashohoxha@gmail.com>
2017-08-28 11:19:25 +0200

    Use the recommended version of docker options.

--- cmd/create.sh
+++ cmd/create.sh
@@ -9,5 +9,5 @@ _EOF
 rename_function cmd_create orig_cmd_create
 cmd_create() {
     orig_cmd_create \
-        -v $(pwd)/tunnels:/home/sshtunnel/tunnels
+        --mount type=bind,src=$(pwd)/tunnels,dst=/home/sshtunnel/tunnels
 }
