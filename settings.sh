328c03f480aefc9b0a203450b98adecc2de0a9c4
dashohoxha <dashohoxha@gmail.com>
2018-05-13 15:09:00 +0200

    Fix comments for gmail account

--- settings.sh
+++ settings.sh
@@ -7,6 +7,8 @@ PROXY_SSH_PORT=22022
 
 PORTS="$PROXY_SSH_PORT:22"
 
-### email account for sending server notifications
+### Gmail account for notifications. This will be used by ssmtp.
+### You need to create an application specific password for your account:
+### https://www.lifewire.com/get-a-password-to-access-gmail-by-pop-imap-2-1171882
 GMAIL_ADDRESS=
 GMAIL_PASSWD=
